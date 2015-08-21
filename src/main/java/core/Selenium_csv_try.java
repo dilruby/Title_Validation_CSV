c75b6a6b44211914e936c3637a91ae73d4561519
Dilrabo Bobokalonova <dilidika@gmail.com>
2015-08-21 00:51:02 -0700

    first commit

---
+++ src/main/java/core/Selenium_csv_try.java
@@ -0,0 +1,71 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium_csv_try {
+	public static void main(String[] a) {
+
+		String csvfile = "./src/main/resources/Test.csv";
+		BufferedReader bufr = null;
+		String line = null;
+		String SplitBy = ",";
+		String test_case_id = null;
+		String url = null;
+		String title_expected = null;
+
+		try {
+			bufr = new BufferedReader(new FileReader(csvfile));
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
+		WebDriver driver = new FirefoxDriver();
+
+		try {
+			while ((line = bufr.readLine()) != null) {
+
+				String[] csv = line.split(SplitBy);
+				test_case_id = csv[0];
+				url = csv[1];
+				title_expected = csv[2];
+
+				driver.get(url);
+				driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				String title_actual = driver.getTitle();
+				System.out.println("");
+				if (title_expected.equals(title_actual)) {
+					System.out.println("TestCase ID: \t\t" + test_case_id);
+					System.out.println("URL: \t\t\t" + url);
+					System.out.println("Title Expected: \t" + title_expected);
+					System.out.println("Title Actual: \t\t" + title_actual);
+					System.out.println("Test Case Result: \t" + "PASSED");
+				} else {
+					System.out.println("Test Case ID: \t\t" + test_case_id);
+					System.out.println("URL: \t\t\t" + url);
+					System.out.println("Title Expected: \t" + title_expected);
+					System.out.println("Title Actual: \t\t" + title_actual);
+					System.out.println("Test Case Result: \t" + "FAILED");
+
+				}
+			}
+
+			bufr.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} finally {
+			driver.quit();
+		}
+
+	}
+
+}
